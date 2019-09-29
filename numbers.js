cac5cb1a3dcff0b3b474b39668b182ad3a249a06
Keanu246 <45147865+Keanu246@users.noreply.github.com>
2019-09-29 21:47:18 +0100

    Update numbers.js

--- numbers.js
+++ numbers.js
@@ -1,5 +1,7 @@
 var notation = true;
 
 document.getElementById("bullets").innerHTML = numberformat.format(bullets)
+document.getElementById("gun").innerHTML = numberformat.format(Math.pow(gunCost))
+
 
 
