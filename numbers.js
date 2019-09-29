b31c79f2c678ee18ee0962cf743685633298da30
Keanu246 <45147865+Keanu246@users.noreply.github.com>
2019-09-29 21:56:56 +0100

    Update numbers.js

--- numbers.js
+++ numbers.js
@@ -1,7 +1,20 @@
-function numbers(){
-
-document.getElementById("bullets").innerHTML = numberformat.format(bullets)
-  
+function valueSet(){ 
+  if(notationScientfic){ 
+      document.getElementById("value").innerHTML = numberformat.format(value, {format: 'scientific'}) 
+  } 
+  else{ 
+      document.getElementById("value").innerHTML = numberformat.format(value); 
+  } 
+} 
+function changeNotation(){ 
+  if(notationScientfic){ 
+    notationScientfic = false; 
+    document.getElementById("notation").innerHTML = "Change Notation to Scientific"; 
+  } 
+  else{ 
+    notationScientfic = true; 
+    document.getElementById("notation").innerHTML = "Change Notation to Standard"; 
+  } 
 }
 
 
