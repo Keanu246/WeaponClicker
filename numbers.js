809da511c9ababfbc5984b33ecd3a0bc40fff4b7
Keanu246 <45147865+Keanu246@users.noreply.github.com>
2019-11-06 23:51:35 +0000

    Update numbers.js

--- numbers.js
+++ numbers.js
@@ -1,21 +1,3 @@
-function bulletSet(){ 
-  if(notationScientfic){ 
-      document.getElementById("bullets").innerHTML = numberformat.format(bullets, {format: 'scientific'}) 
-  } 
-  else{ 
-      document.getElementById("bullets").innerHTML = numberformat.format(bullets); 
-  } 
-} 
-function changeNotation(){ 
-  if(notationScientfic){ 
-    notationScientfic = false; 
-    document.getElementById("notation").innerHTML = "Change Notation to Scientific"; 
-  } 
-  else{ 
-    notationScientfic = true; 
-    document.getElementById("notation").innerHTML = "Change Notation to Standard"; 
-  } 
-}
-  
+npm install --save IdleFormat
 
-
+const numberformat = require('IdleFormat')
